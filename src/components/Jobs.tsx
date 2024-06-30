caf0a119f0f92d416e60b6b6c6692ee41853ea7b
Matt Enterline <matthew.j.enterline@gmail.com>
2024-06-29 22:57:56 -0400

    fixed date string on small screens

--- src/components/Jobs.tsx
+++ src/components/Jobs.tsx
@@ -14,7 +14,7 @@ export function Jobs(props: Props) {
       {jobs.map((job) => (
         <div className="border-2 border-transparent border-solid hover:border-gray-300 rounded-lg p-4 flex flex-col gap-4">
           <div>
-            <div className="flex flex-row justify-between items-baseline">
+            <div className="flex flex-col lg:flex-row justify-between items-baseline">
               <h1 className="text-2xl">{job.company}</h1>
               <span>{getDateString(job.startDate, job.endDate)}</span>
             </div>
