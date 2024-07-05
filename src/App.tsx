81b0c96d9c220288581edfbdc8576af9dc6f0f79
Matt Enterline <matthew.j.enterline@gmail.com>
2024-07-05 08:14:24 -0400

    added padding top so project and blog stays below appbar

--- src/App.tsx
+++ src/App.tsx
@@ -18,12 +18,12 @@ const theme = createTheme({
 });
 function Projects() {
   return (
-    <h2 className="text-2xl text-center mt-8">Projects... Coming Soon!</h2>
+    <h2 className="text-2xl text-center mt-24">Projects... Coming Soon!</h2>
   );
 }
 
 function Blog() {
-  return <h2 className="text-2xl text-center mt-8">Blog... Coming Soon!</h2>;
+  return <h2 className="text-2xl text-center mt-24">Blog... Coming Soon!</h2>;
 }
 function App() {
   return (
